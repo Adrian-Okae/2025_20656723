860a41537e7f1265a925d6c8b9860aed59d20e77
Adrian Okae <adriandelakae@gmail.com>
2026-02-11 09:20:36 +0000

    Worksheet3: static and dynamic libraries complete

--- Worksheet3/adderstatic/include/adder.h
+++ Worksheet3/adderstatic/include/adder.h
@@ -1,6 +1,12 @@
 #ifndef ADDER_H
 #define ADDER_H
 
-int add(int a, int b);
+#ifdef maths_EXPORTS
+#define MATHS_API __declspec(dllexport)
+#else
+#define MATHS_API __declspec(dllimport)
+#endif
+
+MATHS_API int add(int a, int b);
 
 #endif
